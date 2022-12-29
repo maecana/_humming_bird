2dc2a985c034313df0984476d3d309dfbf89d6f5
Mae Anave Caña <mae.cana@outlook.com>
2022-12-30 01:08:03 +0800

    Fetch data from Sanity - Created method to fetch Hums - Created a method to fetch Current User Details - Updated main context config

--- client/context/MainContext.js
+++ client/context/MainContext.js
@@ -7,11 +7,20 @@ export const MainContext = createContext();
 export const MainProvider = ({ children }) => {
     const [appStatus, setAppStatus] = useState('loading');
     const [currentWalletAddress, setCurrentWalletAddress] = useState('');
+    const [hums, setHums] = useState([]);
+    const [currentUserDetails, setCurrentUserDetails] = useState({});
     const router = useRouter();
 
     useEffect(() => {
         isWalletConnected();
     }, []);
+
+
+    useEffect(() => {
+        if(!currentWalletAddress || appStatus !== 'connected') return;
+        getCurrentUserDetails(currentWalletAddress);
+        fetchHums();
+    }, [currentWalletAddress, appStatus]);
 
 
     const isWalletConnected = async () => {
@@ -83,9 +92,73 @@ export const MainProvider = ({ children }) => {
         }
     };
     
+    
+    const fetchHums = async () => {
+        const query = `
+            *[_type == 'hums'] {
+                "bird": bird->{name, walletAddress, profileImage, isProfileImageNFT},
+                hum,
+                timestamp
+            } | order (timestamp desc)
+        `;
+
+        const sanityResponse = await client.fetch(query);
+        setHums([]);
+
+        sanityResponse.forEach(async (items) => {
+            const newItem = {
+                hum: items.hum,
+                timestamp: items.timestamp,
+                bird: {
+                name: items.bird.name,
+                walletAddress: items.bird.walletAddress,
+                isProfileImageNFT: items.bird.isProfileImageNFT,
+                profileImage: items.bird.profileImage,
+                }
+            };
+            setHums(hums => [...hums, newItem]);
+        }); 
+    };
+
+    
+    const getCurrentUserDetails = async (walletAddress = currentWalletAddress) => {
+        if (appStatus !== 'connected') return;
+
+        const query = `
+            *[_type == 'birds' && _id == "${walletAddress}"] {
+                "hums": hums[]->{timestamp, hum} | order (timestamp desc),
+                name,
+                profileImage,
+                isProfileImageNFT,
+                coverImage,
+                walletAddress
+            }
+        `;
+
+        const sanityResponse = await client.fetch(query);
+        setCurrentUserDetails({
+            hums: sanityResponse[0].hums,
+            name: sanityResponse[0].name,
+            profileImage: sanityResponse[0].profileImage,
+            isProfileImageNFT: sanityResponse[0].isProfileImageNFT,
+            coverImage: sanityResponse[0].coverImage,
+            walletAddress: sanityResponse[0].walletAddress
+        });
+    };
+
 
     return (
-        <MainContext.Provider value={{ appStatus, currentWalletAddress, wallectConnect }}>
+        <MainContext.Provider
+            value={{
+                appStatus,
+                currentWalletAddress,
+                hums,
+                currentUserDetails,
+                wallectConnect,
+                fetchHums,
+                getCurrentUserDetails
+            }
+        }>
             {children}
         </MainContext.Provider>
     );
