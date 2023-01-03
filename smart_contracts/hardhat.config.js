d61c658968b28acd3720863324fc5eabfda14699
Mae Anave Caña <mae.cana@outlook.com>
2023-01-04 03:16:44 +0800

    Secure details in hardhat config

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,13 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config({ path: __dirname + '/.env' })
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_KEY=process.env.NEXT_PUBLIC_ALCHEMY_KEY;
+const ALCHEMY_API=`https://eth-goerli.g.alchemy.com/v2/`;
+
 module.exports = {
   solidity: "0.8.9",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/8jEf5MciWc655WKfLJdXmLfdi190DwT2", // Georli Network
+      url: `${ALCHEMY_API}${ALCHEMY_KEY}`, // Goerli Network
       accounts: [
-        `3817187a463e460923d4569bed57b978d34697fe0b7aa28b073079a7f94d613d`,  // Ganache Account - 01
+        process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY,
       ],
     }
   },
